886e6926cfe30c35b37603072d844d56f895b596
Pratheek Shetty <pratheekpramodshetty@gmail.com>
2023-01-16 07:43:34 +0530

    changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,12 +11,12 @@ exports.logout = async(req,res)=>{
     res.status(200).redirect('/')
 }
 
-exports.isLoggedIn= async (req,res,next)=>{
+exports.isLoggedInS= async (req,res,next)=>{
     if(req.cookies.jwt)
     try {
         const decoded = await promisify(jwt.verify)(req.cookies.jwt,process.env.JWT_SECREATE)
 
-        db.query("select * from users where name = ?",[decoded.id],(error,results)=>{
+        db.query("select * from student where usn = ?",[decoded.id],(error,results)=>{
             if(!results){
                 return next()
             }
@@ -26,7 +26,30 @@ exports.isLoggedIn= async (req,res,next)=>{
         })
         
         
+    } catch (error) {
+        console.log(error)
+        return next()
+    }
+    else{
+        next();
+    }
+    
+}
+exports.isLoggedInA= async (req,res,next)=>{
+    if(req.cookies.jwt)
+    try {
+        const decoded = await promisify(jwt.verify)(req.cookies.jwt,process.env.JWT_SECREATE)
 
+        db.query("select * from admin where admin_name = ?",[decoded.id],(error,results)=>{
+            if(!results){
+                return next()
+            }
+    
+            req.users=results[0];
+            return next()
+        })
+        
+        
     } catch (error) {
         console.log(error)
         return next()
@@ -38,33 +61,112 @@ exports.isLoggedIn= async (req,res,next)=>{
 }
 
 
-
-exports.register = (req,res)=>{
+exports.registerS = (req,res)=>{
     console.log(req.body);
 
-    const{name,password,passwordConfirm} = req.body
+    const{name,usn,sec,password,passwordConfirm} = req.body
 
-    db.query('select name from users where name =?',[name],async (error,results)=>{
+    db.query('select usn from student where usn =?',[usn],async (error,results)=>{
         if(error){
             console.log(error)
         }
         if(results.length > 0){
-            return res.render("register",{message:"User Name Already in Use"})
+            return res.render("student_register",{message:"USN Already in Registered"})
         }
         else if(password!==passwordConfirm) {
-            return res.render("register",{message:"Passwords dont Match"})
+            return res.render("student_register",{message:"Passwords dont Match"})
         }
         let hashedPassword = await bcrypt.hash(password,12)
 
-        db.query('insert into users set ?',{name:name,password:hashedPassword},(error,results)=>{
+        db.query('insert into student set ?',{name:name,usn:usn,sec:sec,password:hashedPassword},(error,results)=>{
             if(error){
                 console.log(error);
                 }
             else{
-                return res.render("register",{message:"User registered"})
+                return res.render("student_register",{message:"User registered"})
                 }
         })
     })
+}
+
+exports.registerA = (req,res)=>{
+    console.log(req.body);
+
+    const{admin_name,name,password,passwordConfirm} = req.body
+
+    db.query('select admin_name from admin where admin_name =?',[admin_name],async (error,results)=>{
+        if(error){
+            console.log(error)
+        }
+        if(results.length > 0){
+            return res.render("admin_register",{message:"Admin Name Already in Use"})
+        }
+        else if(password!==passwordConfirm) {
+            return res.render("admin_register",{message:"Passwords dont Match"})
+        }
+        let hashedPassword = await bcrypt.hash(password,12)
+
+        db.query('insert into admin set ?',{admin_name:admin_name,name:name,password:hashedPassword},(error,results)=>{
+            if(error){
+                console.log(error);
+                }
+            else{
+                return res.render("admin_register",{message:"User registered"})
+                }
+        })
+    })
+}
+
+exports.admin_login = async (req,res)=>{
+    console.log(req.body);
+    // const user =req.body.user;
+    // const password = req.body.password
+    try {
+        const{admin_name,password} = req.body
+        
+        if(!admin_name || !password){
+            return res.status(400).render("admin_login",{
+                message:"Please Provide a UserName and Password"
+            })
+        }
+        
+        db.query("select * from admin where admin_name = ?",[admin_name], async(error,results)=>{
+            
+            // console.log(await bcrypt.compare(password, results[0].password))
+            // console.log(password)
+            // console.log(results[0].password)
+            if( !results[0] || !(await bcrypt.compare(password, results[0].password))){
+                res.status(401).render("admin_login",{message:"Email or Password is incorrect"})
+            }
+            else {
+                const id = results[0].admin_name;
+        
+                const token = jwt.sign({id}, process.env.JWT_SECREATE, {
+                expiresIn: process.env.JWT_EXPIRES_IN
+                });
+        
+                
+        
+                const cookieOptions = {
+                expires: new Date(
+                    Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+                ),
+                  httpOnly: true
+                }
+        
+                res.cookie('jwt', token, cookieOptions );
+                 res.status(200).redirect("/admin_dash");
+              }
+        })
+        
+        } 
+    catch (error) {
+        console.log(error)
+        }
+    
+
+    
+
 }
 
 
@@ -77,31 +179,29 @@ exports.register = (req,res)=>{
 
 
 
-
-
-exports.login = async (req,res)=>{
+exports.student_login = async (req,res)=>{
     console.log(req.body);
     // const user =req.body.user;
     // const password = req.body.password
     try {
-        const{name,password} = req.body
+        const{usn,password} = req.body
         
-        if(!name || !password){
-            return res.status(400).render("login",{
+        if(!usn || !password){
+            return res.status(400).render("student_login",{
                 message:"Please Provide a UserName and Password"
             })
         }
         
-        db.query("select * from users where name = ?",[name], async(error,results)=>{
+        db.query("select * from student where usn = ?",[usn], async(error,results)=>{
             
             // console.log(await bcrypt.compare(password, results[0].password))
             // console.log(password)
             // console.log(results[0].password)
             if( !results[0] || !(await bcrypt.compare(password, results[0].password))){
-                res.status(401).render("login",{message:"Email or Password is incorrect"})
+                res.status(401).render("student_login",{message:"Email or Password is incorrect"})
             }
             else {
-                const id = results[0].name;
+                const id = results[0].usn;
         
                 const token = jwt.sign({id}, process.env.JWT_SECREATE, {
                   expiresIn: process.env.JWT_EXPIRES_IN
@@ -117,7 +217,7 @@ exports.login = async (req,res)=>{
                 }
         
                 res.cookie('jwt', token, cookieOptions );
-                 res.status(200).redirect("/student");
+                 res.status(200).redirect("/student_dash");
               }
         })
         
--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,19 @@ const express = require("express")
 const router = express.Router();
 const authController = require("../controllers/auth")
 
-router.post('/register', authController.register)
+router.post('/student_login', authController.student_login)
 
-router.post('/login', authController.login)
+router.post('/admin_login', authController.admin_login)
 // router.get("/student",(req,res)=>{
 //     res.render("student_dash")
 // })
 // router.get("/faculty",(req,res)=>{
 //     res.render("faculty_dash")
 // })
+
+router.post('/student_register', authController.registerS)
+
+router.post('/admin_register', authController.registerA)
+
 router.get('/logout',authController.logout)
 module.exports = router;
